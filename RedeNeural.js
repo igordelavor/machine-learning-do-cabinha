2067cd95d861765b7afc5f7b72238b6ca74c42a3
igordelavor <60244036+igordelavor@users.noreply.github.com>
2020-04-29 17:12:20 -0300

    Update RedeNeural.js

--- RedeNeural.js
+++ RedeNeural.js
@@ -48,53 +48,50 @@ class RedeNeural{
 	//	output.print();
 	//}
 	train(arr, target){
-		let input = Matrix.arrayToMatrix(arr);
-		let hidden = Matrix.multiply(this.weigths_ih, input);
-		
-		hidden = Matrix.add(hidden,this.bias_ih);
-		
-		hidden.map(sigmoid);
-		
-		//HIDDEN -> OUTPUT
-		let output = Matrix.multiply(this.weigths_ho, hidden);
-		output = Matrix.add(output,this.bias_ho);
-		output.map(sigmoid);
-		//output.print();
+		// INPUT -> HIDDEN
+        let input = Matrix.arrayToMatrix(arr);
+        let hidden = Matrix.multiply(this.weigths_ih, input);
+        hidden = Matrix.add(hidden, this.bias_ih);
 
-		// BACKPROPAGATION
+        hidden.map(sigmoid)
 
-		// OUTPUT -> HIDDEN
-		let expected = Matrix.arrayToMatrix(target);
-		let output_error = Matrix.subtract(expected, output);
-		let d_output = Matrix.map(output,d_sigmoid);
-		let hidden_T = Matrix.transpose(hidden);
+        // HIDDEN -> OUTPUT
+        // d(Sigmoid) = Output * (1- Output)
+        let output = Matrix.multiply(this.weigths_ho, hidden);
+        output = Matrix.add(output, this.bias_ho);
+        output.map(sigmoid);
 
-		let gradient = Matrix.hadamard(d_output, output_error);
-		gradient = Matrix.escalar_multiply(gradient, this.learning_rate);
+        // BACKPROPAGATION
 
-		this.bias_ho = Matrix.add(this.bias_ho, gradient);
-		let weigths_ho_deltas = Matrix.multiply(gradient, hidden_T);
-		//weigths_ho_deltas.print();
-		//this.weigths_ho.print();
-		this.weigths_ho = Matrix.add(this.weigths_ho, weigths_ho_deltas);
-		//this.weigths_ho.print();
+        // OUTPUT -> HIDDEN
+        let expected = Matrix.arrayToMatrix(target);
+        let output_error = Matrix.subtract(expected,output);
+        let d_output = Matrix.map(output,dsigmoid);
+        let hidden_T = Matrix.transpose(hidden);
 
+        let gradient = Matrix.hadamard(d_output,output_error);
+        gradient = Matrix.escalar_multiply(gradient,this.learning_rate);
+        
+        // Adjust Bias O->H
+        this.bias_ho = Matrix.add(this.bias_ho, gradient);
+        // Adjust Weigths O->H
+        let weigths_ho_deltas = Matrix.multiply(gradient,hidden_T);
+        this.weigths_ho = Matrix.add(this.weigths_ho,weigths_ho_deltas);
 
-		// HIDDEN -> INPUT
-		let weigths_ho_T = Matrix.transpose(this.weigths_ho);
-		let hidden_error = Matrix.multiply(weigths_ho_T, output_error);
-		let d_hidden = Matrix.map(hidden_error, d_sigmoid);
-		let input_T = Matrix.transpose(input);
+        // HIDDEN -> INPUT
+        let weigths_ho_T = Matrix.transpose(this.weigths_ho);
+        let hidden_error = Matrix.multiply(weigths_ho_T,output_error);
+        let d_hidden = Matrix.map(hidden,dsigmoid);
+        let input_T = Matrix.transpose(input);
 
-		let gradient_H = Matrix.hadamard(hidden_error, d_hidden);
-		gradient_H = Matrix.escalar_multiply(gradient, this.learning_rate);
+        let gradient_H = Matrix.hadamard(d_hidden,hidden_error);
+        gradient_H = Matrix.escalar_multiply(gradient_H, this.learning_rate);
 
-		this.bias_ih = Matrix.add(this.bias_ih, gradient_H);
-
-		let weigths_ih_deltas = Matrix.multiply(gradient, input_T);
-		//weigths_ih_deltas.print();
-		//this.weigths_ih.print();
-		this.weigths_ih = Matrix.add(this.weigths_ih, weigths_ih_deltas);
+        // Adjust Bias O->H
+        this.bias_ih = Matrix.add(this.bias_ih, gradient_H);
+        // Adjust Weigths H->I
+        let weigths_ih_deltas = Matrix.multiply(gradient_H, input_T);
+        this.weigths_ih = Matrix.add(this.weigths_ih, weigths_ih_deltas);
 
 
 	}
